9926923da88355ff3ed04d743d5b5479b8121d4f
Andrey <andy.gusev@list.ru>
2018-02-03 16:20:19 +0300

    Parsing arguements updated

--- main.py
+++ main.py
@@ -5,30 +5,43 @@ from data_parser import read_data
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'create':
+    data_source = sys.argv[1]
+    iterations = int(sys.argv[2])
+    output_filename = sys.argv[3]
+    final_population_filename = None
+    raw_write_period = -1
+    raw_filename = None
+    single_objective = False
+    pos = 4
+    while pos < len(sys.argv):
+        if sys.argv[pos] == '--so':
+            single_objective = True
+        if sys.argv[pos] == '--pop':
+            pos += 1
+            final_population_filename = sys.argv[pos]
+        if sys.argv[pos] == '--snaps':
+            raw_write_period = int(sys.argv[pos + 1])
+            raw_filename = sys.argv[pos + 2]
+            pos += 2
+        pos += 1
+    if data_source == 'create':
         print('Preparing first generation')
-        initial_population = optimize_local.create_initial_population(
+        initial_population = optimize_local.create_population(
             optimize_local.FitnessCalculator(read_data()).calculate_fitness)
         iterations_ran = 0
     else:
         initial_population, iterations_ran = rw.read_population(sys.argv[1])
+    if not single_objective:
         initial_population = optimize_local.make_fronts(initial_population)
         initial_population = optimize_local.crowding_distance_sort(initial_population)
-    iterations = int(sys.argv[2])
-    output_filename = sys.argv[3]
-    if len(sys.argv) > 4:
-        final_population_filename = sys.argv[4]
-    else:
-        final_population_filename = None
-    if len(sys.argv) > 5:
-        raw_write_period = int(sys.argv[5])
-        raw_filename = sys.argv[6]
-    else:
-        raw_write_period = -1
-        raw_filename = None
 
-    best_fitnesses = optimize_local.run(initial_population, iterations, raw_write_period,
-                                        final_population_filename, raw_filename, iterations_ran)
+    if single_objective:
+        best_fitnesses = optimize_local.run_so(initial_population, iterations, raw_write_period,
+                                               final_population_filename, raw_filename, iterations_ran)
+    else:
+        best_fitnesses = optimize_local.run(initial_population, iterations, raw_write_period,
+                                            final_population_filename, raw_filename, iterations_ran)
+
     if iterations_ran == 0:
         fout_params = 'wt'
     else:
